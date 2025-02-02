af1722c4d16bd5a255c231c6828b21cad0f7127e
Leonardo Aguiar <leoagirms@gmail.com>
2025-02-02 20:18:09 -0300

    feat: Add Hello World examples in C and C++

---
+++ hello_world/hello.c
@@ -0,0 +1,9 @@
+#include <stdio.h>
+
+int main() {
+    printf("Hello World!\n");
+    return 0;
+}
+
+// build prompt command:
+// gcc c_test.c -o build/c_test.exe
---
+++ hello_world/hello.cpp
@@ -0,0 +1,9 @@
+#include <iostream>
+
+int main() {
+    std::cout << "Hello, MinGW in C++!" << std::endl;
+    return 0;
+}
+
+// build prompt command:
+// g++ cpp_test.cpp -o build/cpp_test.exe
